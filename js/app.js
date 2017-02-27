3483e5e26454d06c5abb57f42059c998698bc7b0
xChrisDuran <chris.duran1205@gmail.com>
2017-02-27 11:16:06 -0800

    game board made, js logic  written, but needs jQuery and listeners + build on cs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,92 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
   // all code to manipulate the DOM
-  // goes inside this function
+  
+  //var playerX = prompt("")
+var tttCounter = 0;
 
-});
+//winner is player 'x' or player 'o'
+var getChampion = function(){
+	if (winnerIs('x')){
+		console.log("Player x Wins!");                                                          
+			return 'x';
+		} if (winnerIs ('o')) {
+			console.log("Player o Wins!");
+			return 'o';
+		} return null;
+	}
+
+//The winner is... !
+//'player' can be 'x' or 'o'...
+var winnerIs = function (player){
+	return winsRow(player) || winsColum(player) || winsDiagnal(player);
+}
+var allThree = function(player, boxOne, boxTwo, boxThree){
+	return(boxOne === player) && (boxTwo === player) && (boxThree === player);
+}
+
+//player wins by row
+var winsRow = function(player){
+	return allThree(player, boxValue('#a'), boxValue('#b'), boxValue('#c')) ||
+	  allThree(player, boxValue('#d'), boxValue('#e'), boxValue('#f')) ||
+	    allThree(player, boxValue('#g'), boxValue('#h'), boxValue('#i'));
+}
+
+//player wins by column
+var winsColumn = function(player){
+	return allThree(player, boxValue('#a'), boxValue('#d'), boxValue('#g')) ||
+	  allThree(player, boxValue('#b'), boxValue('#e'), boxValue('#h')) ||
+	    allThree(player, boxValue('#c'), boxValue('#f'), boxValue('#i'));
+}
+
+//player wins by diagnal 
+var winsDiagnal = function(player){
+	return allThree(player, boxValue('#a'), boxValue('#e'), boxValue('#i')) ||
+	  allThree(player, boxValue('#c'), boxValue('#e'), boxValue('#g'));
+}
+
+var allThree = function(player, boxOne, boxTwo, boxThree){
+	return (boxOne === player) && (boxTwo === player) && (boxThree === player);
+}
+//way for player to win
+//player x wins by rows. 
+
+/*var xWinsByRow = function(){
+	return tripleXXX(box('a'), box('b'), box('c')) ||
+	  tripleXXX(box('d'), box('e'), box('f')) ||
+	  tripleXXX(box('g'), box('h'), box('i');
+	}
+	var tripleXXX = function (boxOne, boxTwo, boxThree){
+		console.log("Three in a row. You win, X!");
+	}
+
+//player x wins by column
+var oWinsByDiagnal = function(){
+	return tripleOOO(box('a'), box('e'), box('i')) ||
+	  tripleOOO(box('c'), box('e'), box('g'));
+	}
+
+//player o wins by rows.
+var oWinsByRow = function(){
+	return tripleOOO(box('a'), box('b'), box('c')) ||
+	  tripleOOO(box('d'), box('e'), box('f')) ||
+	  tripleOOO(box('g'), box('h'), box('i');
+	}
+	var tripleOOO = function (boxOne, boxTwo, boxThree){
+		console.log("Three in a row. You win, O!");
+	}
+//player o wins by column
+var oWinsByColomn = function(){
+	return tripleOOO(box('a'), box('d'), box('g')) ||
+	  tripleOOO(box('b'), box('e'), box('h')) ||
+	  tripleOOO(box('c'), box('f'), box('i');
+	}
+	//player o wins by column
+var oWinsByDiagnal = function(){
+	return tripleOOO(box('a'), box('e'), box('i')) ||
+	  tripleOOO(box('c'), box('e'), box('g'));
+	}
+*/
+
+*/
+
